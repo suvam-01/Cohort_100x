793205f063da4072b25ad8a8e4a3e15f09daa0e0
Suvam <suvamjha575@gmail.com>
2024-08-06 15:09:57 +0530

    added map and filters

---
+++ week-2.4/filter.js
@@ -0,0 +1,7 @@
+const arr=[1,2,3,4,5];
+
+const brr=arr.filter((i)=>{
+    return i%2==0;
+})
+
+console.log(brr);
---
+++ week-2.4/map.js
@@ -0,0 +1,15 @@
+
+//map returns modified array
+const inp=[1,2,3,4,5];
+
+function mul2(i){
+    return i*2;
+}
+
+let it=inp.map(mul2);
+
+// let it=inp.map(function(i){
+    // i*2
+//});
+
+console.log(it);
